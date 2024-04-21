01478f11e0d2bc74afef60362f6c7c87de7735c2
stepppmum <tashkinova.2017@inbox.ru>
2024-04-21 21:57:26 +0300

    1

--- tests/script_test4.js
+++ tests/script_test4.js
@@ -1,5 +1,6 @@
 let testBtn = document.getElementById('okno');
 let startBtn = document.getElementById('start');
+let space = 32;
 let second = 0;
 let milisec = 0;
 let timer = false;
@@ -8,7 +9,15 @@ let amount = 10;
 const secFinal = document.getElementById('sec');
 const milisecFinal  = document.getElementById('milisec');
 let results = new Array(amount);
+let finalNumber;
 let incorrectAnswer = 0;
+
+
+function block_space(btn) {
+    if (btn.keyCode === space) {
+        btn.preventDefault();
+    }
+}
 
 
 function getRandomInt(min, max) {
@@ -17,16 +26,11 @@ function getRandomInt(min, max) {
 
 
 function startTest() {
-    const colour = getRandomInt(1, 4);
-    if (colour === 1){
-        testBtn.style.backgroundColor = "red";
-    }
-    else if (colour === 2){
-        testBtn.style.backgroundColor = "blue";
-    }
-    else{
-        testBtn.style.backgroundColor = "green";
-    }
+    const firstNumber = getRandomInt(1, 5);
+    const secondNumber = getRandomInt(1, 5);
+    okno.innerHTML = firstNumber + ' + ' + secondNumber;
+    const answer = firstNumber + secondNumber;
+    finalNumber = answer;
     timer = true;
     stopWatch();
     counter++;
@@ -37,21 +41,36 @@ function restartTest() {
     timer = false;
     second = 0;
     milisec = 0;
-    testBtn.style.backgroundColor = "white";
 }
 
+
 function isKeyPressedAndCounterNotZero(event, counter) {
-    if (testBtn.style.backgroundColor === "red"){
-        return event.keyCode === 37 && counter !== 0
-    }
-    if (testBtn.style.backgroundColor === "blue"){
-        return event.keyCode === 38 && counter !== 0
+    console.log(finalNumber);
+    if (finalNumber % 2 === 0){
+        if (event.keyCode === 37 && counter !== 0){
+            return true;
+        }
+        else{
+            incorrectAnswer ++;
+            console.log(incorrectAnswer);
+            return true;
+            //скипает вопрос при неправильном ответе
+        }
+        // стрелочка влево
     }
     else{
-        return event.keyCode === 39 && counter !== 0;
+        if (event.keyCode === 39 && counter !== 0){
+            return true;
+        }
+        else{
+            incorrectAnswer ++;
+            console.log(incorrectAnswer);
+            return true;
+        }
+        // стрелочка впрао
     }
-}    
-
+}
+//переделаю функцию
 
 
 startBtn.addEventListener('keydown', function (event) {
@@ -64,25 +83,6 @@ startBtn.addEventListener('keydown', function (event) {
             results.push(dataSec + dataMilisec);
         } else {
             timer = false;
-        }
-    }
-});
-
-
-startBtn.addEventListener('keydown', function (event) {
-    if (isKeyPressedAndCounterNotZero(event, counter) !== event.keyCode) {
-        if (counter < amount) {
-            restartTest();
-            const clr = setTimeout(startTest, getRandomInt(1, 5) * 1000);
-            // incorrectAnswer ++;
-            // console.log(incorrectAnswer);
-            // let dataSec = document.getElementById('sec').innerHTML;
-            // let dataMilisec = document.getElementById('milisec').innerHTML;
-            // results.push(dataSec + dataMilisec);
-        } else {
-            timer = false;
-            incorrectAnswer ++;
-            console.log(incorrectAnswer);
         }
     }
 });
